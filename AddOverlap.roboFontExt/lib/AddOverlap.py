fe3e843552441ff1bab14450a5bb9f9972fc4eb3
Alexandre Saumier Demers <asaumierdemers@gmail.com>
2014-04-17 21:28:07 +0200

    now works with multiple selection

--- AddOverlap.roboFontExt/lib/AddOverlap.py
+++ AddOverlap.roboFontExt/lib/AddOverlap.py
@@ -1,75 +1,78 @@
 from AppKit import NSImage
+from robofab.pens.pointPen import AbstractPointPen
 from lib.UI.toolbarGlyphTools import ToolbarGlyphTools
 from mojo.events import addObserver
 import math
 import os
 
-offset = 30
-
 def getLength((x1, y1), (x2, y2)):
 	return math.sqrt((x2-x1)**2 + (y2-y1)**2)
-	
 
-def getOffset(offset, (x1, y1), (x2, y2)):
+class AddOverlapPointPen(AbstractPointPen):
 	
-	length = getLength((x1, y1), (x2, y2))
+	offset = 30
 	
-	if length == 0:
-		return None
-	
-	ox = (x2-x1)/length*offset
-	oy = (y2-y1)/length*offset
-	
-	return int(round(ox)), int(round(oy))
-
-
-def offsetSegment(prevSegment, segment, reverse=False):
-	
-	p1 = prevSegment.onCurve
-	p2 = segment.onCurve
-	
-	p1 = p1.x, p1.y
-	p2 = p2.x, p2.y
-	
-	if reverse: # change direction
-		p1, p2 = p2, p1
-	
-	isCurve = False
-	if segment.type == "curve":
+	def __init__(self, selectedPoints=[]):
+		self.selectedPoints = selectedPoints
 		
-		q1, q2 = segment.offCurve
+		self._contours = []
+		self._components = []
+	
+	def beginPath(self):
+		self._contours.append([])
+		self.firstSegment = None
+		self.prevOncurve = None
+	        
+	def addPoint(self, (x, y), segmentType=None, smooth=False, name=None, **kwargs):
+		data = dict(point=(x, y), segmentType=segmentType, smooth=smooth, name=name, kwargs=kwargs)
+		self._contours[-1].append(data) 
+	 
+	def endPath(self):
+		pass
+	
+	def addComponent(self, baseGlyphName, transformation):
+		pass
+	
+	def _offset(self, (x1, y1), (x2, y2)):
+		length = getLength((x1, y1), (x2, y2))
+		ox = (x2-x1)/length*self.offset
+		oy = (y2-y1)/length*self.offset
+		return int(round(ox)), int(round(oy))
+	
+	def drawPoints(self, outpen):
+		for pointsData in self._contours:
+			if len(pointsData) == 1:
+				# ignore single movetos and anchors
+				continue
+			outpen.beginPath()
+			lenPointsData = len(pointsData)
+			for i, pointData in enumerate(pointsData):
+				currentPoint = pointData["point"]
+				addExtraPoint = None
+				if pointData["point"] in self.selectedPoints:
+					prevPointData = pointsData[i-1]
+					nextPointData = pointsData[(i+1) % lenPointsData]
+					
+					prevOffsetX, prevOffsetY = self._offset(prevPointData["point"], pointData["point"])
+					nextOffsetX, nextOffsetY = self._offset(pointData["point"], nextPointData["point"])
+					
+					addExtraPoint = pointData["point"][0] - nextOffsetX, pointData["point"][1] - nextOffsetY
+					
+					currentPoint = currentPoint[0] + prevOffsetX, currentPoint[1] + prevOffsetY
+					
+				outpen.addPoint(currentPoint,
+					pointData["segmentType"],
+					pointData["smooth"],
+					pointData["name"],
+					**pointData["kwargs"]
+				)
+				if addExtraPoint:
+					outpen.addPoint(addExtraPoint, "line")
+				
+			outpen.endPath()
 		
-		q1 = q1.x, q1.y
-		q2 = q2.x, q2.y
-		
-		if reverse:
-			q1, q2 = q2, q1
-		
-		# check that off curves are not overlapping
-		if q2 != p2:
-			isCurve = True
-	
-	if isCurve:
-		
-		x1, y1 = q2
-		x2, y2 = p2
-	
-	else: # is line
-		
-		x1, y1 = p1
-		x2, y2 = p2
-	
-	o = getOffset(offset, (x1, y1), (x2, y2))
-	
-	if o:
-		ox, oy = o
-	else:
-		return
-	
-	if reverse:
-		prevSegment, segment = segment, prevSegment
-	
-	segment.onCurve.move((ox, oy))
+		for baseGlyphName, transformation in self._components:
+			outpen.addComponent(baseGlyphName, transformation)
 
 
 class AddOverlapTool(object):
@@ -115,46 +118,25 @@ class AddOverlapTool(object):
 	
 	def addOverlap(self, sender):
 		
-		glyph = CurrentGlyph()
+		g = CurrentGlyph()
 		
-		selection = [segment for contour in glyph for segment in contour if segment.onCurve.selected]
+		selection = []
 		
-		if len(selection) == 1:
+		for p in g.selection:
+			p.selected = False
+			selection.append((p.x, p.y))
 			
-			glyph.prepareUndo("addOverlap")
-			
-			s = selection[0]
-			
-			contour = s.getParent()
-			
-			point = s.onCurve
-			point.selected = False
-			
-			x, y = point.x, point.y
-			
-			index = contour.segments.index(s)
-			insertIndex = (index+1) % len(contour)
-			
-			contour.insertSegment(insertIndex, "line", [(x, y)])
-			
-			# get the new index from the initial segment
-			index = contour.segments.index(s)
-			
-			indexA = (index-1) % len(contour)
-			indexB = (index+0) % len(contour)
-			indexC = (index+1) % len(contour)
-			indexD = (index+2) % len(contour)
-			
-			segmentA = contour[indexA]
-			segmentB = contour[indexB]
-			segmentC = contour[indexC]
-			segmentD = contour[indexD]
-			
-			offsetSegment(segmentA, segmentB)
-			offsetSegment(segmentC, segmentD, True)
-			
-			glyph.performUndo()
-			
-			glyph.update()
+		pen = AddOverlapPointPen(selection)
+		
+		g.drawPoints(pen)
+		
+		g.prepareUndo("addOverlap")
+		g.clearContours()
+		
+		pen.drawPoints(g.getPointPen())
+		
+		g.performUndo()
+		g.update()
+
 
 AddOverlapTool()
